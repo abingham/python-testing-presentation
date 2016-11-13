4695a254a07270d0a1c7891d62a9e49c44686d35
Austin Bingham <austin@sixty-north.com>
2016-11-13 21:21:45 +0100

    Cleaned up some unittest stuff.

--- examples/unittest_main.py
+++ examples/unittest_main.py
@@ -2,8 +2,11 @@ import unittest
 
 
 class Tests(unittest.TestCase):
+    # This fails trivially
     def test_foo(self):
         self.assertEqual(1, 2)
 
+# This the idiomatic test for "am I the top-level program" in Python
 if __name__ == '__main__':
+    # If this is the top-level pmport unittest
     unittest.main()
