6851290dea5c73b6cfad6bf8215197fc376da8b9
Austin Bingham <austin@sixty-north.com>
2017-01-18 16:28:05 +0000

    Updates prep script.

--- slides/prep_slides.sh
+++ slides/prep_slides.sh
@@ -1,3 +1,7 @@
+# This executes all slides and then clears out their results. This a) serves as
+# a bit of a smoke test, b) ensure that no output from previous runs is sticking
+# around, and c) ensures that all notebooks are trusted.
+
 for NAME in Introduction unittest mock pytest hypothesis cosmic-ray; do
     FNAME=$NAME.ipynb
     echo "*** CONVERTING" $FNAME
